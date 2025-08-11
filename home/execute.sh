886106810c565c9cfc0a5c9fbccd34b058690b0c
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-08-11 22:42:04 +0700

    Update execute.sh - Verify binary exists

--- home/execute.sh
+++ home/execute.sh
@@ -9,6 +9,6 @@ fi
 
 # Run setup and start services
 /app/vikey-inference/vikey-inference-linux nvidia-smi --setup-gpu "GeForce RTX 4090"
-NODE_PORT=11434 DEFAULT_MODEL=llama3.2-8b-instruct nohup /app/vikey-inference/vikey-inference-linux > kuzco.log &
+NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct nohup /app/vikey-inference/vikey-inference-linux > kuzco.log 2>&1 &
 sleep 3
 inference node start --code $CODE
