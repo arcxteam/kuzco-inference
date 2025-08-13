f36d72539157a1dcf4eb6ddfef44fc05317dd7a6
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-08-13 16:36:38 +0700

    execute.sh fix-model w/ floatingPoint16 bit - llama3.2-1b-instruct
    Instance at kuzco dashboard

--- home/execute.sh
+++ home/execute.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# path binary
+# use path binary
 BINARY_PATH="/app/kuzco-inference/vikey-inference/vikey-inference-linux"
 BINARY_DIR="/app/kuzco-inference/vikey-inference"
 
-# Verify binary exists
+# verify binary exists
 if [ ! -f "$BINARY_PATH" ]; then
-    echo "Error: Binary not found! Downloading..."
+    echo "Error: Binary not found! Try Downloading..."
     mkdir -p "$BINARY_DIR"
     cd "$BINARY_DIR"
     wget https://github.com/direkturcrypto/vikey-inference/raw/main/vikey-inference-linux -O vikey-inference-linux
     chmod +x vikey-inference-linux
 fi
 
-# Run setup
+# ran setup
 "$BINARY_PATH" nvidia-smi --setup-gpu "GeForce RTX 4090"
 
-# Start services
+# start services
 cd "$BINARY_DIR"
-NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct nohup ./vikey-inference-linux > kuzco.log 2>&1 &
+NODE_PORT=14444 DEFAULT_MODEL=llama3.2-1b-instruct nohup ./vikey-inference-linux > kuzco.log 2>&1 &
 sleep 3
 inference node start --code $CODE
