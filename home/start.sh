3e59cfbd2e0cd630b47b44ed16d7a64c65ece20a
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-08-13 16:24:12 +0700

    start.sh fix-model w/ floatingPoint16 bit - llama-3.2-1b-instruct
    vikey-low models 2K/1M

--- home/start.sh
+++ home/start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 if [ "$1" == "serve" ]; then
-    # Perbaikan path binary
+    # use path binary
     BINARY_PATH="/app/kuzco-inference/vikey-inference/vikey-inference-linux"
     BINARY_DIR="/app/kuzco-inference/vikey-inference"
     
     if [ ! -f "$BINARY_PATH" ]; then
-        echo "Error: Binary not found! Downloading..."
+        echo "Error: Binary not found! Try Downloading..."
         mkdir -p "$BINARY_DIR"
         cd "$BINARY_DIR"
         wget https://github.com/direkturcrypto/vikey-inference/raw/main/vikey-inference-linux -O vikey-inference-linux
         chmod +x vikey-inference-linux
     fi
     
-    echo "Running Inference using Proxy"
+    echo "Running Inference Model using Proxy"
     cd "$BINARY_DIR"
-    NODE_PORT=14444 DEFAULT_MODEL=llama-3.2-8b-instruct ./vikey-inference-linux > vikey.log 2>&1
+    NODE_PORT=14444 DEFAULT_MODEL=llama-3.2-1b-instruct ./vikey-inference-linux > vikey.log 2>&1
 else
     exit 1
 fi
