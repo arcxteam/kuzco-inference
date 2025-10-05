1e03d2fd7318aefb88fd51132fb8b1174dfb9bef
0xgrey <33593974+arcxteam@users.noreply.github.com>
2025-10-05 23:33:59 +0700

    Update execute.sh default Llama-3.2-3B-Instruct & binary CLI api

--- home/execute.sh
+++ home/execute.sh
@@ -6,10 +6,10 @@ BINARY_DIR="/app/kuzco-inference/vikey-inference"
 
 # verify binary exists
 if [ ! -f "$BINARY_PATH" ]; then
-    echo "Error: Binary not found! Try Downloading..."
+    echo "Error: Binary CLI not found! Try Downloading..."
     mkdir -p "$BINARY_DIR"
     cd "$BINARY_DIR"
-    wget https://github.com/direkturcrypto/vikey-inference/raw/main/vikey-inference-linux -O vikey-inference-linux
+    wget https://github.com/arcxteam/kuzco-inference/releases/download/v1.0.0/vikey-inference-linux -O vikey-inference-linux
     chmod +x vikey-inference-linux
 fi
 
@@ -18,6 +18,6 @@ fi
 
 # start services
 cd "$BINARY_DIR"
-NODE_PORT=14444 DEFAULT_MODEL=llama3.2-1b-instruct nohup ./vikey-inference-linux > kuzco.log 2>&1 &
+NODE_PORT=14444 DEFAULT_MODEL=llama3.2-3b-instruct nohup ./vikey-inference-linux > kuzco.log 2>&1 &
 sleep 3
 inference node start --code $CODE
